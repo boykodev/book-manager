ecbbfc9de97d6666b632c877c0d825e9be26346b
Serge Boyko <boykodev@gmail.com>
2017-10-18 23:52:11 +0300

    add edit action

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Book;
 use AppBundle\Form\BookFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -35,7 +36,31 @@ class AdminController extends Controller
             return $this->redirectToRoute('homepage');
         }
 
-        return $this->render('default/new.html.twig', [
+        return $this->render('admin/new.html.twig', [
+            'bookForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="book_edit")
+     */
+    public function editAction(Request $request, Book $book)
+    {
+        $form = $this->createForm(BookFormType::class, $book);
+
+        // process POST request
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $book = $form->getData();
+
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($book);
+            $manager->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('admin/new.html.twig', [
             'bookForm' => $form->createView()
         ]);
     }
